2ca1965ae50b16fe165e5e60f3dc356a01d124bc
Robert Lanzafame <R.C.Lanzafame@tudelft.nl>
2024-10-13 14:28:26 +0200

    test

--- publishconf.py
+++ publishconf.py
@@ -14,12 +14,15 @@ import sys
 sys.path.append(os.curdir)
 from pelicanconf import *
 
+THEME = "pelican-alchemy/alchemy"
+PLUGIN_PATHS = "pelican-plugins"
+
 # If your site is available via HTTPS, make sure SITEURL begins with https://
 SITEURL = "https://rlanzafame.github.io"
 RELATIVE_URLS = False
 
-FEED_ALL_ATOM = "feeds/all.atom.xml"
-CATEGORY_FEED_ATOM = "feeds/{slug}.atom.xml"
+# FEED_ALL_ATOM = "feeds/all.atom.xml"
+# CATEGORY_FEED_ATOM = "feeds/{slug}.atom.xml"
 
 DELETE_OUTPUT_DIRECTORY = True
 
